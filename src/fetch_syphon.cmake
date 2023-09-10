e3b6c35104a5fa329bb88031636cd58c4aa67b45
Joey <2190436+s6joui@users.noreply.github.com>
2023-09-11 00:30:42 +0900

    removed Syphon from repo, fetching it with cmake

---
+++ src/fetch_syphon.cmake
@@ -0,0 +1,15 @@
+
+# Download Syphon framework
+file(DOWNLOAD
+  https://github.com/Syphon/Syphon-Framework/releases/download/5/Syphon.SDK.5.zip
+  Syphon.zip
+  EXPECTED_HASH SHA256=8a8993da2d39f84b7fbd9ad0071a8f300e947635a52c6c5a521c88df5ccb882f
+  TLS_VERIFY true
+)
+
+file(ARCHIVE_EXTRACT
+    INPUT Syphon.zip
+    DESTINATION .
+    PATTERNS "Syphon SDK 5/Syphon.framework/"
+    VERBOSE
+)
